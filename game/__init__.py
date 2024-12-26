8e7f41ceb539a860ed6e836eb2cf6add1bb00f33
312-23\23 <nams93627@naver.com>
2024-12-26 10:06:54 +0900

    python Package 연습 Class, module, init

---
+++ game/__init__.py
@@ -0,0 +1,13 @@
+from .graphic.render import render_test
+
+VERSION = 3.11
+
+def print_version_info():
+    print(f"The version of this game is {VERSION}")
+    print("Created by JakeANam")
+    print("e-mail: nams93627@naver.com")
+    print("address: Nakamura-ward, Nagoya, Aichi, Japan")
+
+# 패키지 실행 초기화 문구
+print("Initializing game...")
+print("Loading...")
